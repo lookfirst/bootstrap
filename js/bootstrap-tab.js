6fc231a91502571473ee5b02f643af2ca3e46ed1
Jon Stevens <latchkey@gmail.com>
2013-04-05 14:55:04 -0700

    namespace tab events issue https://github.com/jschr/bootstrap-modal/issues/95

--- js/bootstrap-tab.js
+++ js/bootstrap-tab.js
@@ -51,7 +51,7 @@
 
       previous = $ul.find('.active:last a')[0]
 
-      e = $.Event('show', {
+      e = $.Event('show.tab', {
         relatedTarget: previous
       })
 
@@ -64,7 +64,7 @@
       this.activate($this.parent('li'), $ul)
       this.activate($target, $target.parent(), function () {
         $this.trigger({
-          type: 'shown'
+          type: 'shown.tab'
         , relatedTarget: previous
         })
       })
